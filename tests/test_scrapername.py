205e41a6bd672234e9e66c697736df09dd5e71e3
Michael Rans <rans@email.com>
2017-09-15 14:36:00 +0200

    Location

--- tests/test_scrapername.py
+++ tests/test_scrapername.py
@@ -18,7 +18,7 @@ class TestScraperName:
     @pytest.fixture(scope='function')
     def configuration(self):
         Configuration._create(hdx_read_only=True,
-                             project_config_yaml=join('tests', 'config', 'project_configuration.yml'))
+                              project_config_yaml=join('tests', 'config', 'project_configuration.yml'))
         Locations.set_validlocations([{'name': 'afg', 'title': 'Afghanistan'}])  # add locations used in tests
 
     @pytest.fixture(scope='function')
